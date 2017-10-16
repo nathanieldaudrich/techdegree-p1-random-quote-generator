d0cc764ccb0a023aa24f1509e337cbe2272e7bac
Nathaniel Daudrich <naethai@gmail.com>
2017-10-16 18:04:31 +0200

    Initial files to add
    First commit and push

---
+++ js/quotes.js
@@ -0,0 +1,35 @@
+//Quotes from George Ivanovitch Gurdjieff contained in an array of objects
+
+var gigQuotes = [
+    {
+      quote: 'Man has no permanent and unchangeable I. Every thought, every mood, every desire, every sensation, says "I".',
+      source: 'Gurdjieff I. Gurdjieff',
+      citation: 'In Search of the Miraculous, P.D. Ouspensky',
+      year: '1949',
+    },
+    {
+        quote: 'Hope of consciousness is strength. Hope of feelings is slavery. Hope of body is disease.',
+        source: 'Gurdjieff I. Gurdjieff',
+        citation: "All and Everything: Beelzebub's tales to His Grandson, G.I. Gurdjieff",
+        year: '1950',
+      },
+      {
+        quote: 'If you want to lose your faith, make friends with a priest.',
+        source: 'Gurdjieff I. Gurdjieff',
+        citation: "All and Everything: Meetings with Remarkable Men, G.I. Gurdjieff",
+        year: '1963',
+      },
+      {
+        quote: 'By teaching others you will learn yourself.',
+        source: 'Gurdjieff I. Gurdjieff',
+        citation: "Prieuré Study House",
+        year: 'Unknown',
+      },
+      {
+        quote: 'The being of two people can differ from one another more than the being of a mineral and of an animal.',
+        source: 'Gurdjieff I. Gurdjieff',
+        citation: 'In Search of the Miraculous, P.D. Ouspensky',
+        year: '1949',
+      }
+  ];
+  
---
+++ js/script.js
@@ -0,0 +1,47 @@
+//runs printQuote on page load to remove place setter quote in index.html
+document.getElementById("body").addEventListener("load", printQuote, true);
+
+//click on "Show Another Quote" and it initiates the printQuote function.
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+//random quote generator
+function getRandomQuote() {
+    var gurdjieffQuotes;
+    gurdjieffQuotes = gigQuotes[Math.floor(Math.random() * gigQuotes.length)];
+    return gurdjieffQuotes;
+}
+
+//random color generator function for background color using rgb(#,#,#) css
+function getRandomColor() {
+    
+    //set intial rgb
+    var rgbColor = 'rgb'
+    randomR = Math.floor((Math.random() * 255));
+    randomG = Math.floor((Math.random() * 255));
+    randomB = Math.floor((Math.random() * 255));
+    rgbColor += '(' 
+    rgbColor += randomR + ',' 
+    rgbColor +=  randomG + ',' 
+    rgbColor += randomB + ')';
+    return rgbColor;
+}
+
+//function to print quote to index.html
+function printQuote() {
+
+    //defines the getRandomQuote function as as a variable 
+    var gigQuote = getRandomQuote();
+
+    //defines what get output into the html (quote, source, citation and year)   
+    var outputHTML = '<p class="quote">' + gigQuote.quote + '</p>' + '<p class="source">' + gigQuote.source + '</p>';
+        outputHTML += '<span class="citation">' + gigQuote.citation + '</span>';
+        outputHTML += '<span class="year">' + gigQuote.year + '</span>';
+    
+    //fill in the quote box
+    document.getElementById('quote-box').innerHTML = outputHTML;
+
+    //fill in the background-color for #body id
+    document.getElementById("body").style.backgroundColor = getRandomColor();
+    document.getElementById('loadQuote').style.backgroundColor = document.getElementById("body").style.backgroundColor;
+
+}
